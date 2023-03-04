aaa43b712ca6a61769c51ef4caa3d8dfdf7b0ccd
wumian <2202120598@stu.jxufe.edu.cn>
2023-03-04 19:40:27 +0800

    3-4-19:40 修改启动文案

--- src/main/java/com/ydlab/interchoice/InterChoiceApplication.java
+++ src/main/java/com/ydlab/interchoice/InterChoiceApplication.java
@@ -1,14 +1,19 @@
 package com.ydlab.interchoice;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class InterChoiceApplication {
-
+    private static final Logger LOG = LoggerFactory.getLogger(InterChoiceApplication.class);
     public static void main(String[] args) {
-        SpringApplication.run(InterChoiceApplication.class, args);
+        SpringApplication app = new SpringApplication(InterChoiceApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功");
+        LOG.info("地址：\thttp://127.0.0.1:{}",env.getProperty("server.port"));
     }
-
 }
