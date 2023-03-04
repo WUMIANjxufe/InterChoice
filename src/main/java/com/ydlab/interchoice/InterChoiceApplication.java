36bec5aafe54738d4f9c596c3928796c4e9d0cc8
wumian <2202120598@stu.jxufe.edu.cn>
2023-03-04 15:08:19 +0800

    3-4-15:07 init

---
+++ src/main/java/com/ydlab/interchoice/InterChoiceApplication.java
@@ -0,0 +1,13 @@
+package com.ydlab.interchoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterChoiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterChoiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ydlab/interchoice/InterChoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ydlab.interchoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterChoiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
