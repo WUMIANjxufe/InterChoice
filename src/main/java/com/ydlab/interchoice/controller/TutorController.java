00bd914be75d88d200026b29a1ac94bcafa775d7
wumian <2202120598@stu.jxufe.edu.cn>
2023-03-05 22:40:11 +0800

    3-5-22:39 增加通用的返回类CommonResp

--- src/main/java/com/ydlab/interchoice/controller/TutorController.java
+++ src/main/java/com/ydlab/interchoice/controller/TutorController.java
@@ -1,6 +1,7 @@
 package com.ydlab.interchoice.controller;
 
 import com.ydlab.interchoice.domain.Tutor;
+import com.ydlab.interchoice.resp.CommonResp;
 import com.ydlab.interchoice.service.TutorService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +15,10 @@ public class TutorController {
     @Resource
     private TutorService tutorService;
     @GetMapping("/list")
-    public List<Tutor> list(){
-        return tutorService.list();
+    public CommonResp list(){
+        CommonResp<List<Tutor>> resp = new CommonResp<>();
+        List<Tutor> list = tutorService.list();
+        resp.setContent(list);
+        return resp;
     }
 }
