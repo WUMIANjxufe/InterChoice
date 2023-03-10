42504ed78fe7ecc39315675d3fb33edb019cc372
wumian <2202120598@stu.jxufe.edu.cn>
2023-03-10 15:42:33 +0800

    3-10-15:42 集成Ant design Vue,加入按钮示例

--- README.md
+++ README.md
@@ -7,3 +7,16 @@ DEBUG:
 java: 程序包org.springframework.boot不存在的解决方法
 打开idea的设置，在mav的运行程序项里面，勾上【将IDE构建/运行操作委托给Maven】
 
+#SpringBoot端口号被占
+创建新的端口号
+当遇到这个问题时，我们可以创建一个新的端口号来重新执行SpringBoot项目，创建新的端口号其实很简单，只需要在application.yaml或者application.properties文件下配置即可。
+#修改端口号  yaml文件修改
+server:
+port: 8080
+#修改端口号 properties文件配置
+server.port=8080
+2.释放被占有的端口号
+（1）打开 cmd命令窗口，输入netstat -ano|findstr 端口号 命令，查询该端口对应的TCP号；
+查询到端口号后，在关闭对应的TCP号即可关闭端口。
+（2）输入taskkill /t /f /im TCP号，来关闭被占用的端口
+
---
+++ web/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <a-button type="primary">Primary Button</a-button>
+    <a-button>Default Button</a-button>
+    <a-button type="dashed">Dashed Button</a-button>
+    <a-button type="text">Text Button</a-button>
+    <a-button type="link">Link Button</a-button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+    HelloWorld,
+  },
+});
+</script>
