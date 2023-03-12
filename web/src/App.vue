4d2f6098caf420ed5531729316f5b8acd69f327c
wumian <2202120598@stu.jxufe.edu.cn>
2023-03-12 15:17:21 +0800

    3-12-15:17 制作the-footer组件

--- web/src/App.vue
+++ web/src/App.vue
@@ -2,17 +2,17 @@
   <a-layout>
   <TheHeader></TheHeader>
        <router-view/>
-    <a-layout-footer style="text-align: center">
-      师生双选虚拟仿真实验平台
-    </a-layout-footer>
+  <the-footer></the-footer>
   </a-layout>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
 import TheHeader from "@/components/the-header.vue";
+import TheFooter from "@/components/the-footer.vue";
 export default defineComponent({
   name: 'app',
   components:{
+    TheFooter,
     TheHeader
   }
 });
---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    师生双选虚拟仿真实验平台
+  </a-layout-footer>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'the-footer'
+});
+</script>
