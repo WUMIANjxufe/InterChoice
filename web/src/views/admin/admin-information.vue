4e053ba9d292ca28ebfe86d80c21dc963d498e78
wumian <2202120598@stu.jxufe.edu.cn>
2023-03-12 20:26:49 +0800

    3-12-20:26 修改简历管理页面

--- web/src/views/admin/admin-information.vue
+++ web/src/views/admin/admin-information.vue
@@ -1,5 +1,57 @@
 <template>
-  <div class="about">
-    <h1>简历编辑</h1>
-  </div>
+
+  <a-layout>
+    <a-layout-sider width="200" style="background: #fff">
+      <a-menu
+          mode="inline"
+          v-model:selectedKeys="selectedKeys2"
+          v-model:openKeys="openKeys"
+          style="height: 100%"
+      >
+        <a-sub-menu key="sub1">
+          <template #title>
+                <span>
+                  <user-outlined />
+                   入学前
+                </span>
+          </template>
+          <a-menu-item key="information">
+            <router-link to="/admin/information">填写简历</router-link>
+          </a-menu-item>
+          <a-menu-item key="2">选择导师</a-menu-item>
+          <a-menu-item key="3">预测成功率</a-menu-item>
+          <a-menu-item key="4">初选结果</a-menu-item>
+        </a-sub-menu>
+        <a-sub-menu key="sub2">
+          <template #title>
+                <span>
+                  <laptop-outlined />
+                  subnav 2
+                </span>
+          </template>
+          <a-menu-item key="5">option5</a-menu-item>
+          <a-menu-item key="6">option6</a-menu-item>
+          <a-menu-item key="7">option7</a-menu-item>
+          <a-menu-item key="8">option8</a-menu-item>
+        </a-sub-menu>
+        <a-sub-menu key="sub3">
+          <template #title>
+                <span>
+                  <notification-outlined />
+                  subnav 3
+                </span>
+          </template>
+          <a-menu-item key="9">option9</a-menu-item>
+          <a-menu-item key="10">option10</a-menu-item>
+          <a-menu-item key="11">option11</a-menu-item>
+          <a-menu-item key="12">option12</a-menu-item>
+        </a-sub-menu>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout-content :style="{ padding: '0 24px', minHeight: '280px' }">
+      <div class="about">
+        <h1>简历编辑</h1>
+      </div>
+    </a-layout-content>
+  </a-layout>
 </template>
