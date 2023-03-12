148605473b998facefa501dc93b543ee1a74b14f
wumian <2202120598@stu.jxufe.edu.cn>
2023-03-12 15:12:57 +0800

    3-12-15:12 制作the-header组件

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,18 +1,6 @@
 <template>
   <a-layout>
-    <a-layout-header class="header">
-      <div class="logo" />
-      <a-menu
-          theme="dark"
-          mode="horizontal"
-          v-model:selectedKeys="selectedKeys1"
-          :style="{ lineHeight: '64px' }"
-      >
-        <a-menu-item key="1">nav 1</a-menu-item>
-        <a-menu-item key="2">nav 2</a-menu-item>
-        <a-menu-item key="3">nav 3</a-menu-item>
-      </a-menu>
-    </a-layout-header>
+  <TheHeader></TheHeader>
        <router-view/>
     <a-layout-footer style="text-align: center">
       师生双选虚拟仿真实验平台
@@ -20,18 +8,13 @@
   </a-layout>
 </template>
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
+import { defineComponent } from 'vue';
+import TheHeader from "@/components/the-header.vue";
 export default defineComponent({
-  components: {
-
-  },
-  setup() {
-    return {
-      selectedKeys1: ref<string[]>(['2']),
-      selectedKeys2: ref<string[]>(['1']),
-      openKeys: ref<string[]>(['sub1']),
-    };
-  },
+  name: 'app',
+  components:{
+    TheHeader
+  }
 });
 </script>
 <style>
---
+++ web/src/components/the-header.vue
@@ -0,0 +1,22 @@
+<template>
+  <a-layout-header class="header">
+    <div class="logo" />
+    <a-menu
+        theme="dark"
+        mode="horizontal"
+        v-model:selectedKeys="selectedKeys1"
+        :style="{ lineHeight: '64px' }"
+    >
+      <a-menu-item key="1">研一</a-menu-item>
+      <a-menu-item key="2">研二</a-menu-item>
+      <a-menu-item key="3">研三</a-menu-item>
+    </a-menu>
+  </a-layout-header>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'the-header'
+});
+</script>
